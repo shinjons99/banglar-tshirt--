2fc9598c7892433b3bcee29f10e3a63d0c322c15
shinjons99 <shinjons99@gmail.com>
2023-04-09 23:40:00 +0600

    Remove from cart and not allow duplicate

--- src/component/TShirt/TShirt.jsx
+++ src/component/TShirt/TShirt.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './TShirt.css';
 
-const TShirt = ({tshirt}) => {
-    const {picture, name, id}=tshirt;
+const TShirt = ({tshirt, handleAddToCart}) => {
+    const {picture, name, price}=tshirt;
     return (
         <div className='t-shirt'>
             <img src={picture} alt="" />
+            <h4>{name}</h4>
+            <p>Price: ${price}</p>
+            <button onClick={()=> handleAddToCart(tshirt)}>Buy Now</button>
         </div>
     );
 };
