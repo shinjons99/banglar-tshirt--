61b6770d774ada2d8b770d1fdbc87e58a6b28df6
shinjons99 <shinjons99@gmail.com>
2023-04-09 15:13:30 +0600

    after picture

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import TShirt from "../TShirt/TShirt";
+import Cart from "../Cart/Cart";
+import './Home.css';
 
 const Home = () => {
-    const tshirts = useLoaderData();
-    return (
-        <div>
-            <h3>This is Home: {tshirts.length}</h3>
-        </div>
-    );
+  const tshirts = useLoaderData();
+  return (
+    <div className="home-container">
+      <div className="t-shirts-container">
+        {tshirts.map((tshirt) => (
+          <TShirt key={tshirt.id} tshirt={tshirt}></TShirt>
+        ))}
+      </div>
+      <div className="cart-container">
+            <Cart></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
